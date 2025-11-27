53751a5c690019543b2872da0ae30c2a810920ab
Nicolás Rey Alonso <NicolasReyAlonso@gmail.com>
2025-11-27 16:00:43 +0000

    Characters

--- BrainWallGodot/brain-wall/scenes/mainscript.gd
+++ BrainWallGodot/brain-wall/scenes/mainscript.gd
@@ -11,6 +11,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if mp.has_camera_texture():
-		plane.material_override.albedo_texture = mp.get_camera_texture()
 	pass
